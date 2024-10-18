ab6c7895013d12c5586b1f8a382027454184179b
Caio Santana de Moura <caiosantanaprofissional@gmail.com>
2024-10-18 01:59:27 -0300

    criar arquivo css para o header, importar header para a home page e desenvolver componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import logo from "../assets/logo.svg"
+import bgLogo from "../assets/image-1.png"
+import "../styles/header.css"
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className='headerDiv'>
+            <img src={logo} className='logo' width={216} height={143} />
+            <img src={bgLogo} className='bgLogo' />
+        </div>
+    )
 }
 
 export default Header
